736045da0b85ab2bef69256bcb40d767ce0a1537
Sam Wyndham <samwyndham@users.noreply.github.com>
2024-08-29 16:27:01 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "Down",
+    platforms: [
+        .iOS("15.0"),
+    ],
+    products: [
+        .library(
+            name: "Down",
+            targets: ["Down"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "libcmark",
+            dependencies: [],
+            path: "libcmark",
+            exclude: [
+              "include",
+            ],
+            publicHeadersPath: "./"
+        ),
+        .target(
+            name: "Down",
+            dependencies: ["libcmark"],
+            path: "Source",
+            exclude: ["Down.h"],
+          resources: [
+            .copy("../Resources/DownView.bundle"),
+          ]
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
+)
